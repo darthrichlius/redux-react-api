3186363cc89a91f184f83e888195aebd9f94d7c2
darthrichlius <hello@rdieud.com>
2024-06-30 20:14:02 +0200

    feat(server): bugs - create patch, post and delete routes

--- server/src/routes/bug.routes.ts
+++ server/src/routes/bug.routes.ts
@@ -8,4 +8,62 @@ router.get("/", async (_req: Request, res: Response) => {
   res.json(bugs);
 });
 
+router.patch("/:id/resolve", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const _id = parseInt(id);
+    if (!_id) throw new Error(":id parameter must be from type number");
+
+    const updated = await prismaClient.bug.update({
+      where: {
+        id: _id,
+      },
+      data: {
+        resolved: true,
+      },
+    });
+    res.json(updated);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: (err as Error).message,
+    });
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    const bug = req.body;
+    const added = await prismaClient.bug.create({
+      data: bug,
+    });
+    res.json(added);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: (err as Error).message,
+    });
+  }
+});
+
+router.delete("/:id", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const _id = parseInt(id);
+    if (!_id) throw new Error(":id parameter must be from type number");
+
+    await prismaClient.bug.delete({
+      where: {
+        id: _id,
+      },
+    });
+    res.sendStatus(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      error: (err as Error).message,
+    });
+  }
+});
+
 export default router;
